7b584df4008525e97d675ac38198694075326498
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-06-16 23:28:54 -0500

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,19 @@
+import * as path from 'path';
 import adapter from '@sveltejs/adapter-auto';
-
+import { imagetools } from 'vite-imagetools';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
+	},
+	vite: {
+		plugins: [ imagetools() ],
+
+		resolve: {
+			alias: {
+				$img: path.resolve('src/images')
+			}
+		}
 	}
 };
 
