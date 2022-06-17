7494e26c0b4f3360fada616130745712f9a21aee
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-06-16 22:53:24 -0500

    bug

--- src/stores.js
+++ src/stores.js
@@ -7,7 +7,7 @@ export const productsList = [
         description:
             'The MacBook is carved out of solid aluminum, thus giving it a distinctive look and a grayish-white hue. The MacBook Air in particular has a thin and light design. There are two versions of the MacBook Air: the 11-inch, with an 11.6-inch screen; and the 13-inch, with a 13.3-inch screen.',
         imageUrl:
-            '/mac.png'
+            './assets/mac.png'
     },
     {
         id: 2,
@@ -16,6 +16,6 @@ export const productsList = [
         description:
             'The iPhone is a smartphone made by Apple that combines a computer, iPod, digital camera and cellular phone into one device with a touchscreen interface.',
         imageUrl:
-            '/apple.png'
+            './assets/apple.png'
     }
 ];
